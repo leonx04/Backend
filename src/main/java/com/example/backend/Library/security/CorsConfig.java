e24aff9052662518f61cc1ec52ebd5dd742e58b6
Test Git <sonptph43120@gmail.com>
2024-11-19 21:41:46 +0700

    update code 19/11/2024

---
+++ src/main/java/com/example/backend/Library/security/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.backend.Library.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5501", "http://127.0.0.1:5500"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
